8765d93cb2832cf5f0cfa951edc7f651f8257d79
Leonardo <leo.bonfim.95@gmail.com>
2024-09-26 17:21:23 -0300

    chore: Add validation on listen config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`🚀 HTTP server is running on port ${env.PORT}`)
-})
+app
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 HTTP server is running on port ${env.PORT}`)
+  })
